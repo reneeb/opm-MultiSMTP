8fbb08c656ecdfaa8d4a4f537d9c40406d230cc9
Shadryn <i.shadryn@novacom.by>
2017-11-28 13:34:32 +0300

    fix translation

--- Kernel/Language/ru_MultiSMTP.pm
+++ Kernel/Language/ru_MultiSMTP.pm
@@ -1,11 +1,11 @@
 # --
 # Kernel/Language/ru_MultiSMTP.pm - the Russian translation of MultiSMTP
-# Copyright (C) 2011-2017 Perl-Services.de, http://perl-services.de/
-# Copyright (C) 2017 WestDevTeam, http://westdev.by/
+# Copyright (C) 2011-2017 Perl-Services.de; http://perl-services.de/
+# Copyright (C) 2017 WestDevTeam; http://westdev.by/
 # --
-# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# This software comes with ABSOLUTELY NO WARRANTY. For details; see
 # the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# did not receive this file; see http://www.gnu.org/licenses/agpl.txt.
 # --
 
 package Kernel::Language::ru_MultiSMTP;
@@ -17,33 +17,30 @@ use utf8;
 sub Data {
     my $Self = shift;
 
-    $Self->{Translation} = {
+    # Kernel/Config/Files/MultiSMTP.xml
+    $Self->{Translation}->{'Frontend module registration for the MultiSMTP interface.'} = 'Регистрация модуля MultiSMTP.';
+    $Self->{Translation}->{'Manage SMTP settings.'} = 'Менеджер SMTP настроек.';
+    $Self->{Translation}->{'SMTP settings'} = 'SMTP настройки';
+    $Self->{Translation}->{'The salt for password encryption. It has to have exactly 8 bytes!'} = 'Соль для шифрования паролей. Он должен иметь ровно 8 байтов!';
+    $Self->{Translation}->{'The key for password encryption.'} = 'Ключ для шифрования паролей.';
+    $Self->{Translation}->{'The fallback mechanism when MultiSMTP fails.'} = 'Механизм возврата при сбое MultiSMTP.';
+    $Self->{Translation}->{'Enable debugging mode of MultiSMTP.'} = 'Включить режим отладки MultiSMTP.';
+    $Self->{Translation}->{'Debug MultiSMTP'} = 'Отладка MultiSMTP';
+    $Self->{Translation}->{'Debug MultiSMTP and Net::SMTP'} = 'Отладка MultiSMTP и Net::SMTP';
 
-        # Kernel/Config/Files/MultiSMTP.xml
-        'Frontend module registration for the MultiSMTP interface.' => 'Регистрация модуля MultiSMTP.',
-        'Manage SMTP settings.' => 'Менеджер SMTP настроек.',
-        'SMTP settings' => 'SMTP настройки',
-        'The salt for password encryption. It has to have exactly 8 bytes!' => 'Соль для шифрования паролей. Он должен иметь ровно 8 байтов!',
-        'The key for password encryption.' => 'Ключ для шифрования паролей.',
-        'The fallback mechanism when MultiSMTP fails.' => 'Механизм возврата при сбое MultiSMTP.',
-        'Enable debugging mode of MultiSMTP.' => 'Включить режим отладки MultiSMTP.',
-        'Debug MultiSMTP' => 'Отладка MultiSMTP',
-        'Debug MultiSMTP and Net::SMTP' => 'Отладка MultiSMTP и Net::SMTP',
+    # Kernel/Output/HTML/Templates/Standard/AdminMultiSMTPForm.tt
+    $Self->{Translation}->{'SMTP Management'} = 'Управление SMTP';
+    $Self->{Translation}->{'Add/Change SMTP'} = 'Добавить/изменить SMTP';
+    $Self->{Translation}->{'Anonymous SMTP'} = 'Анонимный SMTP';
+    $Self->{Translation}->{'Emails'} = 'Электронные почтовые адреса';
 
-        # Kernel/Output/HTML/Templates/Standard/AdminMultiSMTPForm.tt
-        'SMTP Management' => 'Управление SMTP',
-        'Add/Change SMTP' => 'Добавить/изменить SMTP',
-        'Anonymous SMTP' => 'Анонимный SMTP',
-        'Emails' => 'Электронные почтовые адреса',
-
-        # Kernel/Output/HTML/Templates/Standard/AdminMultiSMTPList.tt
-        'Add SMTP' => 'Добавить SMTP',
-        'ID' => 'ID',
-        'Creator' => 'Создатель',
-        'No matches found.' => 'Совпадений не найдено.',
-        'edit' => 'редактировать',
-        'delete' => 'удалить',   
-    };
+    # Kernel/Output/HTML/Templates/Standard/AdminMultiSMTPList.tt
+    $Self->{Translation}->{'Add SMTP'} = 'Добавить SMTP';
+    $Self->{Translation}->{'ID'} = 'ID';
+    $Self->{Translation}->{'Creator'} = 'Создатель';
+    $Self->{Translation}->{'No matches found.'} = 'Совпадений не найдено.';
+    $Self->{Translation}->{'edit'} = 'редактировать';
+    $Self->{Translation}->{'delete'} = 'удалить';
 }
 
-1,
+1;
